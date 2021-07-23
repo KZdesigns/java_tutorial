f977268bc5c9a3a5408a8fbb61dce5d3d20220b5
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-07-22 19:08:55 -0700

    first commit

---
+++ hello.java
@@ -0,0 +1,15 @@
+/**
+ * hello
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+        double z = 56;
+        double sum = x * y * z;
+        double power = Math.pow(sum, y);
+
+        System.out.println(power);
+    }
+}
