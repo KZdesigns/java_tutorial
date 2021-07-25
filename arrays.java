699a5f7f3c039641431c84c2a6bc309cd4a2c105
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-07-24 18:09:24 -0700

    arrays complete

---
+++ arrays.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * arrays
+ */
+public class arrays {
+
+    public static void main(String[] args) {
+        String[] newArr = new String[5];
+        newArr[0] = "Kyle";
+        newArr[1] = "Lauriane";
+        newArr[2] = "Joe";
+        newArr[3] = "Tyler";
+        newArr[4] = "Wrigley";
+
+        String x = newArr[2];
+
+        int[] nums = {10,9,8,7,6,5,4,3,2,1};
+
+        int y = nums[0];
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
